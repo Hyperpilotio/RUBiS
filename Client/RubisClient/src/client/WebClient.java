c40650db6bcd2ce463c3b8cee35bbaef2305fe86
thorhsu <thorshu@gmail.com>
2017-09-15 12:59:04 +0800

    RubisClient added

---
+++ Client/RubisClient/src/client/WebClient.java
@@ -0,0 +1,47 @@
+package client;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import edu.rice.rubis.client.ClientEmulator;
+
+/**
+ * Servlet implementation class WebClient
+ */
+@WebServlet("/WebClient")
+public class WebClient extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public WebClient() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html; charset=UTF-8");
+		response.setCharacterEncoding("UTF-8");
+		String host = request.getParameter("host");
+		String port = request.getParameter("port");
+		ClientEmulator.main(new String[]{host, port}, response);
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
